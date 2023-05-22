9d58f3b1798f3c9cd946f5f7989a114ec42c617e
HarishKumark <khharish2@gmail.com>
2023-05-22 17:47:43 +0100

    Create ClimbingStairs.java

---
+++ ClimbingStairs.java
@@ -0,0 +1,26 @@
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+
+        int res = climbStairs(45);
+        System.out.println(res);
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int[] a = new int[n];
+        a[0] = 1;
+        a[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+        return a[n - 1];
+    }
+}
